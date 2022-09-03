e0b75fb0c58c549e6a90e5219c96b74c93130ffa
Nakade Shoya <doshouya@gmail.com>
2022-09-03 17:17:30 +0900

    Initial Commit

---
+++ CryptoQuize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoQuize
+//
+//  Created by 中出翔也 on 2022/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoQuize/CryptoQuizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoQuizeApp.swift
+//  CryptoQuize
+//
+//  Created by 中出翔也 on 2022/09/03.
+//
+
+import SwiftUI
+
+@main
+struct CryptoQuizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
